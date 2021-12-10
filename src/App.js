2da734cc34bd718693b07915bbc7ba05645ca988
shahzodkuchkarov <shahzodk0@gmail.com>
2021-12-10 15:49:49 +0500

    refactored appjs

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
-import React from 'react';
-import { Redirect, Route, Switch, Link } from 'react-router-dom';
-import ProtectedRoutes from './containers/Auth/ProtectedRoutes';
-import { publicURLs, protectedURLs } from './routes';
-import Sidebar from './containers/Sidebar';
-import Login from './containers/Auth/Login11';
-import SignUp from './containers/Auth/SignUp';
-import { useState } from 'react';
+import React from "react";
+import { Redirect, Route, Switch, Link } from "react-router-dom";
+import ProtectedRoutes from "./containers/Auth/ProtectedRoutes";
+import { protectedURLs } from "./routes";
+import Sidebar from "./containers/Sidebar";
+import Login from "./containers/Auth/Login11";
+import SignUp from "./containers/Auth/SignUp";
 export default function App(props) {
-  const token = 'dsa';
-  console.log(token)
-  const [state, setstate] = useState(null)
-
   if (localStorage.token) {
     return (
       <>
-        <Sidebar
-          buttonText="Sign out"
-          icon={false}
-        />
+        <Sidebar buttonText="Sign out" icon={false} />
         <div className="main-content">
           <Switch>
-            {
-              protectedURLs.map((item, index) => <ProtectedRoutes key={index} {...item} />)
-            }
+            {protectedURLs.map((item, index) => (
+              <ProtectedRoutes key={index} {...item} />
+            ))}
           </Switch>
         </div>
       </>
-    )
+    );
   }
 
   return (
     <div>
       <Switch>
-        <Route path='/sign-up'>
+        <Route path="/sign-up">
           <SignUp></SignUp>
         </Route>
-        <Route >
-          <Login login={() => setstate()} ></Login>
+        <Route>
+          <Login></Login>
         </Route>
       </Switch>
     </div>
-  )
+  );
 }
--- src/containers/Auth/SignUp.js
+++ src/containers/Auth/SignUp.js
@@ -1,15 +1,14 @@
-import React, { useState, useCallback } from 'react'
-import { Sign } from '../../style/SignUp'
-import Axios from "../../utils/axios"
-import { useHistory } from 'react-router-dom';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import Image from '../../assets/auth/ice-cream.svg';
-import Loader from '../../components/Loader'
-
+import React, { useState, useCallback } from "react";
+import { Sign } from "../../style/SignUp";
+import Axios from "../../utils/axios";
+import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Image from "../../assets/auth/ice-cream.svg";
+import Loader from "../../components/Loader";
 
 export default function SignUp() {
   const history = useHistory();
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
   const [values, setValues] = useState({
@@ -18,58 +17,80 @@ export default function SignUp() {
     email: "",
     phone: "",
     password: "",
-    image: ""
   });
 
   const handleInputChange = useCallback((e) => {
     const { name, value } = e.target;
-    setValues(state => ({ ...state, [name]: value }));
+    setValues((state) => ({ ...state, [name]: value }));
   }, []);
 
   const handelSubmit = (e) => {
     e.preventDefault();
-    setLoading(true)
-    Axios.post('/auth/sign-up', values)
+    setLoading(true);
+    Axios.post("/auth/sign-up", values)
       .then(({ data }) => {
         if (data.success) {
-          console.log('success', values)
-          history.push('/login')
-          setLoading(false)
-        }
-
-        else {
-          setError(data.error)
-          setLoading(false)
+          console.log("success", values);
+          history.push("/login");
+          setLoading(false);
+        } else {
+          setError(data.error);
+          setLoading(false);
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-        setError(err.response?.data?.msg)
-        setLoading(false)
-      })
-  }
+        setError(err.response?.data?.msg);
+        setLoading(false);
+      });
+  };
   return (
     <Sign>
       <Loader loading={loading} />
       <img src={Image} alt="" />
       <form onSubmit={handelSubmit}>
         <h1>Sign Up</h1>
-        {
-          error ? <div className="error">{error}</div> : null
-        }
-        <input placeholder="First name" onChange={handleInputChange} type="text" name="firstName" />
-        <input placeholder="Last name" onChange={handleInputChange} type="text" name="lastName" />
-        <input placeholder="Email" onChange={handleInputChange} type="email" name="email" />
-        <input placeholder="Phone number" onChange={handleInputChange} type="number" name="phone" />
-        <input placeholder="Password" onChange={handleInputChange} required type="password" name="password" minlength="6" />
+        {error ? <div className="error">{error}</div> : null}
+        <input
+          placeholder="First name"
+          onChange={handleInputChange}
+          type="text"
+          name="firstName"
+        />
+        <input
+          placeholder="Last name"
+          onChange={handleInputChange}
+          type="text"
+          name="lastName"
+        />
+        <input
+          placeholder="Email"
+          onChange={handleInputChange}
+          type="email"
+          name="email"
+        />
+        <input
+          placeholder="Phone number"
+          onChange={handleInputChange}
+          type="number"
+          name="phone"
+        />
+        <input
+          placeholder="Password"
+          onChange={handleInputChange}
+          required
+          type="password"
+          name="password"
+          minlength="6"
+        />
         {/* <input type="text" name="image" /> */}
-        <button onChange={handleInputChange} type="submit">Sign Up</button>
+        <button onChange={handleInputChange} type="submit">
+          Sign Up
+        </button>
         <div>
           Already have an account? <Link to="/login">Sign in</Link>
         </div>
-
-
       </form>
     </Sign>
-  )
+  );
 }
